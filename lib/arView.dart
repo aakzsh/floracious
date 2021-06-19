6f244f52946bfb8d8bcd50656da721f339115f5b
shruti <shrutigupta5555@gmail.com>
2021-06-19 21:50:31 +0530

    ADD: Cylinder in ar view

--- lib/arView.dart
+++ lib/arView.dart
@@ -15,10 +15,11 @@ class _ArViewState extends State<ArView> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello World'),
+          title: const Text('AR'),
         ),
         body: ArCoreView(
           onArCoreViewCreated: _onArCoreViewCreated,
+          enableTapRecognizer: true,
         ),
       ),
     );
@@ -27,53 +28,22 @@ class _ArViewState extends State<ArView> {
   void _onArCoreViewCreated(ArCoreController controller) {
     arCoreController = controller;
 
-    _addSphere(arCoreController);
-    _addCylindre(arCoreController);
-    _addCube(arCoreController);
+    _addCylinder(arCoreController);
   }
 
-  void _addSphere(ArCoreController controller) {
-    final material = ArCoreMaterial(color: Color.fromARGB(120, 66, 134, 244));
-    final sphere = ArCoreSphere(
-      materials: [material],
-      radius: 0.1,
-    );
-    final node = ArCoreNode(
-      shape: sphere,
-      position: vector.Vector3(0, 0, -1.5),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  void _addCylindre(ArCoreController controller) {
+  void _addCylinder(ArCoreController controller) {
     final material = ArCoreMaterial(
       color: Colors.red,
       reflectance: 1.0,
     );
-    final cylindre = ArCoreCylinder(
+    final cylinder = ArCoreCylinder(
       materials: [material],
       radius: 0.5,
       height: 0.3,
     );
     final node = ArCoreNode(
-      shape: cylindre,
-      position: vector.Vector3(0.0, -0.5, -2.0),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  void _addCube(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Color.fromARGB(120, 66, 134, 244),
-      metallic: 1.0,
-    );
-    final cube = ArCoreCube(
-      materials: [material],
-      size: vector.Vector3(0.5, 0.5, 0.5),
-    );
-    final node = ArCoreNode(
-      shape: cube,
-      position: vector.Vector3(-0.5, 0.5, -3.5),
+      shape: cylinder,
+      position: vector.Vector3(0.0, -1.5, -2.0),
     );
     controller.addArCoreNode(node);
   }
