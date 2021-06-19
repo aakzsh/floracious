fea77a163270171c7893512675b59565c5a74056
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-06-19 13:00:56 +0530

    fix routing error

--- lib/main.dart
+++ lib/main.dart
@@ -12,104 +12,116 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Floracious",
-      home: Scaffold(
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Image.asset(
-                "assets/logo.png",
-                height: 150,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Divider(color: Colors.white),
-              ),
-              GridView.count(
-                crossAxisCount: 2,
-                primary: true,
-                shrinkWrap: true,
+        debugShowCheckedModeBanner: false,
+        title: "Floracious",
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Image.asset(
+              "assets/logo.png",
+              height: 90,
+            ),
+            Container(
+              height: 280,
+              width: 280,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.all(20),
-                    child: AspectRatio(
-                        aspectRatio: 1,
-                        child: MaterialButton(
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ARView()));
-                          },
-                          child: Icon(Icons.vrpano_rounded),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                        )),
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                    child: Divider(color: Colors.white),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: AspectRatio(
-                        aspectRatio: 1,
-                        child: MaterialButton(
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Reminder()));
-                          },
-                          child: Icon(Icons.notifications),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                        )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      InkWell(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            height: 100,
+                            width: 100,
+                            child: Center(
+                              child: Icon(
+                                Icons.send_to_mobile_rounded,
+                                size: 50,
+                              ),
+                            ),
+                          )),
+                      InkWell(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            height: 100,
+                            width: 100,
+                            child: Center(
+                              child: Icon(Icons.notifications, size: 50),
+                            ),
+                          ))
+                    ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: AspectRatio(
-                        aspectRatio: 1,
-                        child: MaterialButton(
-                          color: Colors.white,
-                          onPressed: () {
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      InkWell(
+                          onTap: () {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) => Search()));
                           },
-                          child: Icon(Icons.search),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                        )),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: AspectRatio(
-                        aspectRatio: 1,
-                        child: MaterialButton(
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Compass()));
-                          },
-                          child: Icon(Icons.directions),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                        )),
-                  ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            height: 100,
+                            width: 100,
+                            child: Center(
+                              child: Icon(Icons.search, size: 50),
+                            ),
+                          )),
+                      InkWell(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            height: 100,
+                            width: 100,
+                            child: Center(
+                              child: Icon(Icons.compare_arrows_sharp, size: 50),
+                            ),
+                          ))
+                    ],
+                  )
                 ],
               ),
-            ],
-          ),
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Colors.green,
-            Colors.greenAccent,
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+            )
+          ],
         ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromRGBO(108, 151, 114, 1),
+          Color.fromRGBO(11, 59, 45, 1),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
       ),
     );
   }
