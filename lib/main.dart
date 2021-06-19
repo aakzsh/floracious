61534ca711aa201e5e2bd157f708e61f93d45f6e
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-06-19 21:32:09 +0530

    compass and weather

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:floracious/compass.dart';
 import 'package:floracious/reminder.dart';
 import 'package:floracious/search.dart';
 import 'package:flutter/material.dart';
-import 'arView.dart';
+import 'package:geolocator/geolocator.dart';
+
+Position position;
 
 void main() {
   runApp(MyApp());
@@ -26,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    _determinePosition();
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -50,7 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
                       InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            // Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) => ()));
+                          },
                           child: Container(
                             decoration: BoxDecoration(
                                 color: Colors.white,
@@ -65,7 +73,12 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           )),
                       InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Reminder()));
+                          },
                           child: Container(
                             decoration: BoxDecoration(
                                 color: Colors.white,
@@ -99,7 +112,12 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           )),
                       InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Compass()));
+                          },
                           child: Container(
                             decoration: BoxDecoration(
                                 color: Colors.white,
@@ -107,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             height: 100,
                             width: 100,
                             child: Center(
-                              child: Icon(Icons.compare_arrows_sharp, size: 50),
+                              child: Icon(Icons.directions, size: 50),
                             ),
                           ))
                     ],
@@ -125,4 +143,45 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  position = await Geolocator.getCurrentPosition();
+
+  print(position);
+
+  return Geolocator.getCurrentPosition();
 }
--- lib/search.dart
+++ lib/search.dart
@@ -1,13 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+import 'main.dart';
 
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
 }
 
+String citylol;
+double templol;
+
 class _SearchState extends State<Search> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  String key = '856822fd8e22db5e1ba48c0e7d69844a';
+  WeatherFactory ws;
+  List<Weather> _data = [];
+  double lat, lon;
+  String city = "Fetching..";
+  double temp = 0;
+  weather() async {
+    Weather weather = await ws.currentWeatherByLocation(
+        position.latitude, position.longitude);
+    setState(() {
+      citylol = weather.areaName;
+      templol = weather.temperature.celsius;
+      temp = templol;
+      city = citylol;
+      print(_data);
+      super.dispose();
+    });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    ws = new WeatherFactory(key);
+  }
+
+  Widget build(BuildContext context) {
+    weather();
+
+    return Scaffold(
+        body: Container(
+      color: Color.fromRGBO(229, 229, 229, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.location_city),
+                      Text("$city")
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.cloud, color: Colors.yellow),
+                      Text("$temp")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(20),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: TextField(),
+                  height: 35,
+                  width: 250,
+                  color: Colors.white,
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.search))
+              ],
+            ),
+          ),
+          Container(
+              height: 400,
+              color: Colors.transparent,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[plant(), plant(), plant(), plant()],
+                ),
+              ))
+        ],
+      ),
+    ));
+  }
+}
+
+plant() {
+  return ListTile(
+    leading: Icon(Icons.nature),
+    title: Text("Plant's Name"),
+    subtitle: Text("Information about plants"),
+  );
 }
