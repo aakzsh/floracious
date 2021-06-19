8a9f5447e726a05f3f8bcd9baa0d50e98a27d555
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-06-19 06:23:47 +0530

    homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Floracious",
       home: Scaffold(
         body: Container(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text("Floracious",
-                  style: TextStyle(color: Colors.white, fontSize: 40)),
+              Image.asset(
+                "assets/logo.png",
+                height: 150,
+              ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 30),
                 child: Divider(color: Colors.white),
@@ -29,74 +32,82 @@ class MyApp extends StatelessWidget {
                 primary: true,
                 shrinkWrap: true,
                 children: <Widget>[
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: MaterialButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ARView()));
-                        },
-                        child: Icon(Icons.vrpano_rounded),
-                        height: 150,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: MaterialButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Reminder()));
-                        },
-                        child: Icon(Icons.notifications),
-                        height: 150,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: MaterialButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Search()));
-                        },
-                        child: Icon(Icons.search),
-                        height: 150,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  AspectRatio(
-                      aspectRatio: 1,
-                      child: MaterialButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Compass()));
-                        },
-                        child: Icon(Icons.compare_arrows_sharp),
-                        height: 150,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: AspectRatio(
+                        aspectRatio: 1,
+                        child: MaterialButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ARView()));
+                          },
+                          child: Icon(Icons.vrpano_rounded),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: AspectRatio(
+                        aspectRatio: 1,
+                        child: MaterialButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Reminder()));
+                          },
+                          child: Icon(Icons.notifications),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: AspectRatio(
+                        aspectRatio: 1,
+                        child: MaterialButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Search()));
+                          },
+                          child: Icon(Icons.search),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: AspectRatio(
+                        aspectRatio: 1,
+                        child: MaterialButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Compass()));
+                          },
+                          child: Icon(Icons.directions),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
                 ],
               ),
             ],
           ),
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            Color.fromARGB(1, 11, 59, 45),
-            Color.fromARGB(1, 108, 151, 114),
+            Colors.green,
+            Colors.greenAccent,
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         ),
       ),
