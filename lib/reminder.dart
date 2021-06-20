2602b5b206c218081b56bd1ccb0475b27789b2da
shruti <shrutigupta5555@gmail.com>
2021-06-20 12:00:14 +0530

    add: 80% reminder uwu

--- lib/reminder.dart
+++ lib/reminder.dart
@@ -4,6 +4,10 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
+import 'package:intl/intl.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 FlutterLocalNotificationsPlugin notificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
@@ -15,10 +19,46 @@ class Reminder extends StatefulWidget {
 class _ReminderState extends State<Reminder> {
   TextEditingController hoursController = TextEditingController();
   TextEditingController minsController = TextEditingController();
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   void submit() {
     int hh = int.parse(hoursController.text);
     int mm = int.parse(minsController.text);
+
+    displayNotification(hh, mm);
+    storeTimeInfo(hh, mm);
+  }
+
+  var counter = 0;
+  List<String> rem = [];
+
+  void loadTimeInfo() async {
+    final SharedPreferences prefs = await _prefs;
+    final savedNumber = await prefs.getStringList("reminder").toList() ?? [];
+    // print(savedNumber);
+    for (var i = 0; i < savedNumber.length; i++) {
+      var m = savedNumber[i];
+      var formatstr = DateTime.parse(m);
+      var month = DateFormat().add_yMMMM().format(formatstr);
+      var time = DateFormat().add_jm().format(formatstr);
+      print(month);
+      print(time);
+    }
+  }
+
+  void storeTimeInfo(int hh, int mm) async {
+    final SharedPreferences prefs = await _prefs;
+
+    var now = DateTime.now();
+    var notifTime = now.add(Duration(hours: hh, minutes: mm));
+    rem.add(notifTime.toString());
+    // print(rem);
+    await prefs.setStringList("reminder", rem);
+    // var month = DateFormat().add_yMMMM().format(notifTime);
+    // var time = DateFormat().add_jm().format(notifTime);
+    // print(time);
+    // print(month);
+    loadTimeInfo();
   }
 
   void initState() {
@@ -63,7 +103,7 @@ class _ReminderState extends State<Reminder> {
               ],
             ),
             TextButton(
-              onPressed: () => displayNotification(),
+              onPressed: () => submit(),
               child: Text('Set Reminder'),
             )
           ],
@@ -73,13 +113,15 @@ class _ReminderState extends State<Reminder> {
   }
 }
 
-Future<void> displayNotification() async {
+Future<void> displayNotification(int hh, int mm) async {
+  print(mm);
+  print(hh);
   notificationsPlugin.zonedSchedule(
       0,
-      'Zanie needs food, attention pwease',
-      'Where are you?',
+      'Eureka needs water',
+      'Time to water your plants',
       tz.TZDateTime.now(tz.local).add(
-        Duration(seconds: 5),
+        Duration(hours: hh, minutes: mm),
       ),
       NotificationDetails(
           android: AndroidNotificationDetails(
